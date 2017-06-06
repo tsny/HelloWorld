1e5854269dd20ca3deb3847d6633f68fcc003fb1
Taylor Snyder <tsny@Taylors-MacBook-Pro.local>
2017-06-06 02:31:44 -0500

    Added button functionality and finished assignment

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var infoLabel: UILabel!
+    
+    @IBAction func helloClick(_ sender: Any)
+    {
+        infoLabel.text = "Hello World!"
+    }
+    
+    @IBAction func clearClick(_ sender: Any)
+    {
+        infoLabel.text = nil
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
